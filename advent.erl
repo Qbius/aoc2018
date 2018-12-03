1aa7c78ba0c10ecfcd456bd9a8cc63106849798e
msurowka <theqbius@gmail.com>
2018-12-03 18:59:11 +0100

    no "last day" shenanigans

--- advent.erl
+++ advent.erl
@@ -1,5 +1,5 @@
 -module(advent).
--export([start/0]).
+-export([get/1]).
 
 get_last_day() ->
     lists:nth(1, lists:reverse(lists:sort(filelib:wildcard("days/day*.erl")))).
@@ -10,16 +10,16 @@ compile(Filename) ->
             code:purge(Module),
             code:load_file(Module),
             Module;
-        {error, _Errors} ->
+        error ->
             false
     end.
 
-start() ->
-    Filename = get_last_day(),
-    io:fwrite(case compile(Filename) of
+get(DayNumber) ->
+    Filename = "days/day" ++ integer_to_list(DayNumber) ++ ".erl",
+    case compile(Filename) of
         false -> 
             "Couldn't compile " ++ Filename;
         Module ->
             {Easy, Hard} = Module:answer(), 
             io_lib:format("1st part: ~p~n2nd part: ~p~n", [Easy, Hard])
-    end).
+    end.
