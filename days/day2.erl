d0a6d6e2dcfb8ee31862e521f7018892620bdf7b
msurowka <theqbius@gmail.com>
2018-12-03 18:52:56 +0100

    day2 adjustments to match refactor

--- days/day2.erl
+++ days/day2.erl
@@ -1,13 +1,10 @@
 -module(day2).
--export([easy/0, hard/0]).
+-export([answer/0]).
 
-easy() ->
+answer() ->
     {ok, Contents} = file:read_file("input/day2"),
-    {TwosTotal, ThreesTotal} = lists:foldl(fun(BinLine, {Twos, Threes}) ->
-        Counts = maps:values(char_map(binary_to_list(BinLine))),
-        {Twos + case lists:member(2, Counts) of true -> 1; false -> 0 end, Threes + case lists:member(3, Counts) of true -> 1; false -> 0 end}
-    end, {0, 0}, string:split(Contents, <<"\n">>, all)),
-    TwosTotal * ThreesTotal.
+    Lines = [binary_to_list(BinLine) || BinLine <- string:split(Contents, <<"\n">>, all)],
+    {get_checksum(Lines), find_different_by_one_character(Lines)}.
 
 char_map(String) ->
     lists:foldl(fun(C, CharMap) ->
@@ -19,9 +16,14 @@ char_map(String) ->
         end
     end, #{}, String).
 
-hard() ->
-    {ok, Contents} = file:read_file("input/day2"),
-    Lines = [binary_to_list(BinLine) || BinLine <- string:split(Contents, <<"\n">>, all)],
+get_checksum(Lines) ->
+    {TwosTotal, ThreesTotal} = lists:foldl(fun(Line, {Twos, Threes}) ->
+        Counts = maps:values(char_map(Line)),
+        {Twos + case lists:member(2, Counts) of true -> 1; false -> 0 end, Threes + case lists:member(3, Counts) of true -> 1; false -> 0 end}
+    end, {0, 0}, Lines),
+    TwosTotal * ThreesTotal.
+
+find_different_by_one_character(Lines) ->    
     lists:usort(lists:filtermap(fun
         RemoveDifferentChar({[H | R1], [H | R2], Count, Result}) ->
             RemoveDifferentChar({R1, R2, Count, [H | Result]});
